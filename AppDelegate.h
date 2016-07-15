975cf4556b85dda75fdabe58a432b20bab0896fc
gdevi6878 <gayathridevikotapati@Gayathris-MacBook-Pro.local>
2016-07-15 15:27:42 -0700

    facebook
    integration

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fb
+//
+//  Created by Gayathri devi Kotapati on 7/15/16.
+//  Copyright © 2016 Gayathri devi Kotapati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  fb
+//
+//  Created by Gayathri devi Kotapati on 7/15/16.
+//  Copyright © 2016 Gayathri devi Kotapati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
